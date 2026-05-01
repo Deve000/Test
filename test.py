d65d92d9e55b45d47f5c57a62dfdc540e75c8c87
Deve000 <rebuta.devesophia912@gmail.com>
2026-05-01 20:53:35 +0800

    main menu and conditional statements

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 print("Hello, World!")
 
-name = int("\nWhat name would you like me to use? ")
-print(f"Got it. Hi there, {name}!")
+name = input("\nWhat name would you like me to use? ")
+print(f"\nGot it. Hi there, {name}!")
+
+print("\nMenu:")
+print("1. Enter world")
+print("2. Exit world")
+
+choice = int(input("\nWhat would you like to do today? "))
+
+if choice == 1:
+    print("\n>> Entering world, please standby...")
+elif choice == 2:
+    print("\n>> Exiting world, see you again!")
+else:
+    print("Choice unavailable, please choose again~")
